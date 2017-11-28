a580e9812ac5ed25778c013a7dbc14056a5c05b9
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-11-27 22:56:49 -0500

    Add NLEntity

---
+++ src/NLEntity.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class NLEntity extends Component {
+  render() {
+    return <strong className="nl-entity">{this.props.children}</strong>;
+  }
+}
+
+NLEntity.propTypes = {
+  children: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  salience: PropTypes.number.isRequired,
+  sentiment: PropTypes.shape({
+    magnitude: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired
+  })
+};
+
+export default NLEntity;
--- src/NLText.js
+++ src/NLText.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import NLCategory from "./NLCategory";
+import NLEntity from "./NLEntity";
+import reactStringReplace from "react-string-replace";
 
 class NLText extends Component {
   categories() {
     if (this.props.categories.length) {
       return this.props.categories.map(category => (
-        <NLCategory {...category} />
+        <NLCategory key={category.name} {...category} />
       ));
     }
+  }
+
+  annotatedText(text) {
+    let replacedText = text;
+
+    this.props.entities.forEach(entity => {
+      replacedText = reactStringReplace(replacedText, entity.name, match => (
+        <NLEntity {...entity}>{match}</NLEntity>
+      ));
+    });
+
+    return replacedText;
+  }
+
+  annotatedChildren() {
+    return React.Children.map(this.props.children, child =>
+      // TODO: Handle case where child is not a string (ie. a node with HTML tags within it)
+      // We should be able to loop through it like we do here, maybe reusing this method
+      this.annotatedText(child.props.children)
+    );
   }
 
   render() {
     return (
       <div>
-        {this.props.children}
+        {this.annotatedChildren()}
         {this.categories()}
       </div>
     );
@@ -26,6 +48,7 @@ NLText.defaultProps = {
 };
 
 NLText.propTypes = {
+  children: PropTypes.node.isRequired,
   categories: PropTypes.arrayOf(PropTypes.shape(NLCategory.propTypes)),
   entities: PropTypes.arrayOf(
     PropTypes.shape({
